fd8d19d5bef46eac09d39ee31d69d169e3be80ca
sakshikosta <73442804+sakshikosta@users.noreply.github.com>
2020-12-05 01:24:44 +0000

    Fibonacci series added

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-full_name = input('Please input your full name: ')
-print(len(full_name))
-
-n = int(input('Please enter an integer: '))
-while n > 1 and n <= 7:
-    if n == 1:
-        print('Monday')
-    elif n == 2:
-        print('Tuesday')
-    elif n == 3:
-        print('Wednesday')
-    elif n == 4:
-        print('Thursday')
-    elif n == 5:
-        print('Friday')
-    elif n == 6:
-        print('Saturday')
-    else:
-        print('Sunday')
-    n = int(input('Please enter an integer: '))
-print('The provided integer does not correspond to a day!')
+n = int (input("Provide the n-th number for the Fibonacci series: "))
+a, b = 0, 1
+while b < n:
+    print(b)
+    a, b = b, a + b
